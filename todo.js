230327f164f24e9b7036a3d8912e930c40e722c9
TaylorCharlesHall <139002121+TaylorCharlesHall@users.noreply.github.com>
2023-10-26 17:54:07 -0400

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,10 @@
+function Todo({todo, index, remove}) {
+    function handle(){
+        remove(index);
+    }
+    return (
+        <div className="todo" onClick={handle}>[] {index+1} {todo.text} </div>
+    );
+
+
+}
